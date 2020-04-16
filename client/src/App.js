128d5c7d364215c04f6062b59587aa59942b4f66
Dawson415 <dawson.m.schaffer@outlook.com>
2020-04-16 12:44:35 -0700

    Working with first component.....

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ApolloCliect from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 import logo from './logo.png';
 import './App.css';
+import Launches from './components/Launches';
+const client = new ApolloCliect({
+  uri: 'http://localhost:5000/graphql'
+});
 
 function App() {
   return (
-    <div className="container">
-      <img src={logo} alt="SpaceX" style={{ width: 300, display: 'block', margin: 'auto' }} />
-    </div>
+    <ApolloProvider client={client}>
+      <div className="container">
+        <img src={logo} alt="SpaceX" style={{ width: 300, display: 'block', margin: 'auto' }} />
+      </div>
+      <Launches />>
+    </ApolloProvider>
   );
 }
 
