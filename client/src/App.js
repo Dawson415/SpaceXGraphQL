adac2eed92428bf59dd69797b155e69e346fb508
Dawson415 <dawson.m.schaffer@outlook.com>
2020-04-16 18:48:39 -0700

    --- Auto-Git Commit ---

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import ApolloCliect from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import logo from './logo.png';
 import './App.css';
 import Launches from './components/Launches';
+import Launch from './components/Launch';
 
 const client = new ApolloCliect({
   uri: 'http://localhost:5000/graphql'
@@ -17,6 +18,7 @@ function App() {
         <div className="container">
           <img src={logo} alt="SpaceX" style={{ width: 300, display: 'block', margin: 'auto' }} />
           <Route exact path="/" component={Launches} />
+          <Route exact path="/launch/:flight_number" component={Launch} />
           <Launches />
         </div>
       </Router>
