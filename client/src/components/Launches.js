054e1374c219f605b6aad319e7bd1b458ebfe719
Dawson415 <dawson.m.schaffer@outlook.com>
2020-04-17 07:55:41 -0700

    Added first Fragment piece to Launches...

--- client/src/components/Launches.js
+++ client/src/components/Launches.js
@@ -18,16 +18,20 @@ const LAUNCHES_QUERY = gql`
 export class Launches extends Component {
     render() {
         return (
-            <div>
+            <Fragment>
                 <h1 className="display-4 my-3">Launches</h1>
                 <Query query={LAUNCHES_QUERY}>
                     {({ loading, error, data }) => {
                         if (loading) return <h4>Loading...</h4>;
                         if (error) console.log(error);
-                        return <h1>Test</h1>;
+                        return (
+                            <Fragment>
+                                <h1>Test</h1>
+                            </Fragment>
+                        );
                     }}
                 </Query>
-            </div>
+            </Fragment>
         );
     }
 }
