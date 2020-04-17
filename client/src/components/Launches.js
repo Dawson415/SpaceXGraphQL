ddddb81380661cb86d2a4e595fab3252ef7d99f7
Dawson415 <dawson.m.schaffer@outlook.com>
2020-04-17 07:45:48 -0700

    Working starting on success/failure ledgend....

--- client/src/components/Launches.js
+++ client/src/components/Launches.js
@@ -18,27 +18,39 @@ const LAUNCHES_QUERY = gql`
 export class Launches extends Component {
     render() {
         return (
-            <Fragment>
+            <div>
                 <h1 className="display-4 my-3">Launches</h1>
-                <MissionKey />
                 <Query query={LAUNCHES_QUERY}>
-                    {
-                        ({ loading, error, data }) => {
-                            if (loading) return <h4>Loading...</h4>
-                            if (error) console.log(error);
-                            console.log(data.launches);
-                            return (<Fragment><h3>Test</h3></Fragment>)
-                            // <Fragment>
-                            //     {data.launches.map(launch => (
-                            //         <LaunchItem key={launch.flight_number} launch={launch} />
-                            //     ))}
-                            // </Fragment>;
-                        }
-                    }
+                    {({ loading, error, data }) => {
+                        if (loading) return <h4>Loading...</h4>;
+                        if (error) console.log(error);
+                        return <h1>Test</h1>;
+                    }}
                 </Query>
-            </Fragment>
-        )
+            </div>
+        );
     }
 }
+
+// <Fragment>
+//     <h1 className="display-4 my-3">Launches</h1>
+//     <MissionKey />
+//     <Query query={LAUNCHES_QUERY}>
+//         {
+//             ({ loading, error, data }) => {
+//                 if (loading) return <h4>Loading...</h4>
+//                 if (error) console.log(error);
+//                 console.log(data.launches);
+//                 return (<Fragment>{data.launches.map(launch => ())}</Fragment>)
+//                 // <Fragment>
+//                 //     {data.launches.map(launch => (
+//                 //         <LaunchItem key={launch.flight_number} launch={launch} />
+//                 //     ))}
+//                 // </Fragment>;
+//             }
+//         }
+//     </Query>
+// </Fragment>
+
 
 export default Launches;
