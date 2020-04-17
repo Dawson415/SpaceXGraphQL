0e680d1363eeefdaa004c762840d18f792320bbd
Dawson415 <dawson.m.schaffer@outlook.com>
2020-04-16 18:18:34 -0700

    --- Auto-Git Commit ---

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ApolloCliect from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import logo from './logo.png';
 import './App.css';
 import Launches from './components/Launches';
@@ -12,10 +13,13 @@ const client = new ApolloCliect({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="container">
-        <img src={logo} alt="SpaceX" style={{ width: 300, display: 'block', margin: 'auto' }} />
-        <Launches />
-      </div>
+      <Router>
+        <div className="container">
+          <img src={logo} alt="SpaceX" style={{ width: 300, display: 'block', margin: 'auto' }} />
+          <Route exact path="/" component={Launches} />
+          <Launches />
+        </div>
+      </Router>
     </ApolloProvider>
   );
 }
